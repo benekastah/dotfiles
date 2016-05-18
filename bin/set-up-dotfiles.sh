5354c8aa071126dd2cdcfb03ac1cd0c8e5dda731
Paul Harper <benekastah@gmail.com>
2016-05-17 21:59:15 -0700

    Update set-up-dotfiles.sh

--- bin/set-up-dotfiles.sh
+++ bin/set-up-dotfiles.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 git clone --bare git@github.com:benekastah/dotfiles.git $HOME/.dotfiles
-alias config='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-config checkout
-config config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
