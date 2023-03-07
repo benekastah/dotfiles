9f165b769b72d9725655d541f1118939e329a137
Paul Harper <paul@pathlight.com>
2023-03-07 15:32:51 -0800

    Update set-up-dotfiles.sh

--- bin/set-up-dotfiles.sh
+++ bin/set-up-dotfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git clone --bare git@github.com:benekastah/dotfiles.git $HOME/.dotfiles
-git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
 git --git-dir=$HOME/.dotfiles --work-tree=$HOME checkout
+git --git-dir=$HOME/.dotfiles --work-tree=$HOME config --local status.showUntrackedFiles no
