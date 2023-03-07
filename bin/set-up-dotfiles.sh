7ff00fc3ffa9bb0cbaff39ee00ca6f9788dd76e7
Paul Harper <paul@pathlight.com>
2023-03-07 15:49:58 -0800

    Update set-up-dotfiles.sh

--- bin/set-up-dotfiles.sh
+++ bin/set-up-dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone --bare git@github.com:benekastah/dotfiles.git $HOME/.dotfiles
-alias g="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+g() { git --git-dir=$HOME/.dotfiles --work-tree=$HOME" "$@"; }
 g add .
 g commit -m "$hostname initialize"
 g tag pre-install
